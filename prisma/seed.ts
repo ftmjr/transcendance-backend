7ef0be20add624bbcf2dface45d0bc4dbd38a55e
Josias Kasongo <josiaskas@gmail.com>
2023-05-30 11:44:15 -0400

    update schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,41 +1,43 @@
 // prisma/seed.ts
 
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Role } from '@prisma/client';
 
 // initialize Prisma Client
 const prisma = new PrismaClient();
 
 async function main() {
-    const user1 = await prisma.user.upsert({
-        where: { id: 0 },
-        update: {},
-        create: {
-            id: 0,
-            name: "flahoud",
-            email: 'flahoud@email.com',
-        },
-    });
+  const user1 = await prisma.user.upsert({
+    where: { id: 0 },
+    create: {
+      username: 'flahoud',
+      email: 'flahoud@email.com',
+      password: 'pass1234',
+      role: Role.ADMIN,
+    },
+    update: {},
+  });
 
-    const user2 = await prisma.user.upsert({
-        where: { id: 1 },
-        update: {},
-        create: {
-            id: 1,
-            name: "flahoud2",
-            email: 'flahoud2@email.com',
-        },
-    });
+  const user2 = await prisma.user.upsert({
+    where: { id: 1 },
+    create: {
+      username: 'flahoud2',
+      email: 'flahoud2@email.com',
+      password: 'pass1234',
+      role: Role.SUPER_MODERATOR,
+    },
+    update: {},
+  });
 
-    console.log({ user1, user2 });
+  console.log({ user1, user2 });
 }
 
 // execute the main function
 main()
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    })
-    .finally(async () => {
-        // close Prisma Client at the end
-        await prisma.$disconnect();
-    });
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
