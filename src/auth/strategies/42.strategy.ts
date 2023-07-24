cb50cc5ec1de2799fd002818731bacb20ed60b1f
Francis Lahoud <flahoud@student.42quebec.com>
2023-07-24 18:10:00 -0400

    Update 42 strategy to init avatar properly

--- src/auth/strategies/42.strategy.ts
+++ src/auth/strategies/42.strategy.ts
@@ -1,28 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-42';
+import { Profile, Strategy, VerifyCallback } from 'passport-42';
 import { AuthService } from '../auth.service';
 import { OauthData } from '../interfaces';
-
-interface Profile42 {
-    id: string;
-    displayName: string;
-    name: { familyName: string; givenName: string };
-    emails: Array<{ value: string; verified: boolean }>;
-    photos: Array<{ value: string }>;
-    provider: string;
-    _raw: string;
-    _json: {
-        sub: string;
-        name: string;
-        given_name: string;
-        family_name: string;
-        picture: string;
-        email: string;
-        email_verified: boolean;
-        locale: string;
-    };
-}
 
 @Injectable()
 export class Strategy42 extends PassportStrategy(Strategy, '42') {
@@ -37,13 +17,12 @@ export class Strategy42 extends PassportStrategy(Strategy, '42') {
     async validate(
         accessToken: string,
         refreshToken: string,
-        profile: Profile42,
+        profile: Profile,
         done: VerifyCallback,
     ): Promise<any> {
-        const { id, emails, name, photos } = profile;
+        const { id, emails, name, _json } = profile;
         // generate a username based on the 42 profile, will only be used if the user doesn't have a username yet
         const generatedUsername = `42-${name.givenName}-${name.familyName}`;
-
         // transform the 42 profile into our OauthData interface
         const data: OauthData = {
             id,
@@ -52,7 +31,7 @@ export class Strategy42 extends PassportStrategy(Strategy, '42') {
             profile: {
                 firstName: name.givenName,
                 lastName: name.familyName,
-                avatar: photos[0].value,
+                avatar: _json.image.link,
             },
             from: '42',
         };
