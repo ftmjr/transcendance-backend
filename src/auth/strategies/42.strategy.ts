6d9a3a8a501548fe5ca401d7c3dd9a1dff430bfa
Francis Lahoud <flahoud@student.42quebec.com>
2023-08-04 18:18:13 -0400

    Update 42.strategy.ts
    redirect on .env URL instead of IP

--- src/auth/strategies/42.strategy.ts
+++ src/auth/strategies/42.strategy.ts
@@ -6,36 +6,37 @@ import { OauthData } from '../interfaces';
 
 @Injectable()
 export class Strategy42 extends PassportStrategy(Strategy, '42') {
-    constructor(private authService: AuthService) {
-        super({
-            clientID: process.env.API42_CLIENT_ID,
-            clientSecret: process.env.API42_CLIENT_SECRET,
-            callbackURL:'https://10.18.200.130/api/auth/42/callback',
-        });
-    }
+  constructor(private authService: AuthService) {
+    const callback = 'https://' + process.env.URL + '/api/auth/42/callback';
+    super({
+      clientID: process.env.API42_CLIENT_ID,
+      clientSecret: process.env.API42_CLIENT_SECRET,
+      callbackURL: callback,
+    });
+  }
 
-    async validate(
-        accessToken: string,
-        refreshToken: string,
-        profile: Profile,
-        done: VerifyCallback,
-    ): Promise<any> {
-        const { id, emails, name, _json } = profile;
-        // generate a username based on the 42 profile, will only be used if the user doesn't have a username yet
-        const generatedUsername = `42-${name.givenName}-${name.familyName}`;
-        // transform the 42 profile into our OauthData interface
-        const data: OauthData = {
-            id,
-            email: emails[0].value,
-            username: generatedUsername,
-            profile: {
-                firstName: name.givenName,
-                lastName: name.familyName,
-                avatar: _json.image.link,
-            },
-            from: '42',
-        };
-        const user = await this.authService.createOrFindUserWithOauthData(data);
-        done(null, user);
-    }
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { id, emails, name, _json } = profile;
+    // generate a username based on the 42 profile, will only be used if the user doesn't have a username yet
+    const generatedUsername = `42-${name.givenName}-${name.familyName}`;
+    // transform the 42 profile into our OauthData interface
+    const data: OauthData = {
+      id,
+      email: emails[0].value,
+      username: generatedUsername,
+      profile: {
+        firstName: name.givenName,
+        lastName: name.familyName,
+        avatar: _json.image.link,
+      },
+      from: '42',
+    };
+    const user = await this.authService.createOrFindUserWithOauthData(data);
+    done(null, user);
+  }
 }
