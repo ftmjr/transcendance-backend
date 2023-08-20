0f798cafc78cf3a4c5fc8266c89355b3d2fbc3a0
Francis Lahoud <flahoud@student.42quebec.com>
2023-08-20 02:18:00 -0400

    files upload

---
+++ src/files/files.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { FilesService } from './files.service';
+import { FilesController } from './files.controller';
+import { UsersModule } from "../users/users.module";
+
+@Module({
+  controllers: [FilesController],
+  providers: [FilesService],
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
+    UsersModule,
+  ],
+})
+export class FilesModule {}
