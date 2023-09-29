b31557f7832c5a383eab3a1e01a620498ed1c004
Josias Kasongo <josiaskas@gmail.com>
2023-09-28 20:30:06 -0400

    check for uploads read write

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ npx prisma migrate deploy
 # Execute Prisma seed
 npx prisma db seed
 
+# set uploads folder permissions
+chmod -R 777 /app/uploads
+
 echo "Executed Prisma deploy, initializing server..."
 
 exec "$@"
