74f4a816698e5e85fb8bd6d2518ed7d9dc4b5f59
Josias Kasongo <josiaskas@gmail.com>
2023-11-07 18:02:14 -0500

    change routes for files to relative

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const serverBaseUrl = 'https://localhost/api/uploads';
+const serverBaseUrl = '/api/uploads';
 async function main() {
   try {
     const awardPromises = [
